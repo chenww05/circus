db5549ef953af743a4e4e5bb69c5ac03264ee1e7
Nick Chen <nick@turingvideo.com>
2023-06-14 13:56:20 -0700

    use yarn to resolve conflict

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
+import SignIn from './components/SignIn';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="contained" color="primary" size="large">
+        Submit
+      </Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <SignIn ></SignIn>
     </div>
   );
 }
